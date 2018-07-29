1908672cdef4f91b0b929ef843aec6c1b790bac0
admin <admin@admins-MacBook-Pro-2.local>
2018-07-29 14:09:47 -0700

    update

--- wikipedia/wikipedia.py
+++ wikipedia/wikipedia.py
@@ -357,7 +357,7 @@ def categorysearchtimestamp(title, starttime, endtime, results=500):
     'cmend': endtime,
     'cmsort': 'timestamp',
     'cmprop': 'ids|title|type|timestamp',
-    'cmnamspace': 0
+    'cmnamspace': '0'
   }
 
   last_continue = {}
