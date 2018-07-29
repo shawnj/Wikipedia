f66152a05ec5aa41517924b572d9219b26ccfd0c
admin <admin@admins-MBP-2.hsd1.wa.comcast.net>
2018-07-29 15:35:52 -0700

    update

--- biographies.py
+++ biographies.py
@@ -91,7 +91,7 @@ def main():
         try:
             requests.packages.urllib3.disable_warnings()
             print(r)
-            p = wikipedia.page(title=None,pageid=r['pageid'])
+            p = wikipedia.page(title=None,pageid=str(r['pageid']))
             table_data.update({r:{'PAGEID': str(p.pageid),'TOUCHED': str(p.touched), 'URL': str(p.url)}})
         except Exception as e:
             print("Error: %s" % e)
