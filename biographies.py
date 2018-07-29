b91959ee3bbd0e91e4465bc50911f5b52a444327
admin <admin@admins-MBP-2.hsd1.wa.comcast.net>
2018-07-29 15:47:51 -0700

    update

--- biographies.py
+++ biographies.py
@@ -103,7 +103,7 @@ def main():
     values = [x for x in table_data.values()]
 
     for index, t in enumerate(keys):
-        _revs = [r for r in get_revisions(str(t)) if "delet" in str(r)]
+        _revs = [r for r in get_revisions(str(t),title=title) if "delet" in str(r)]
         _task = create_task(str(DATASET_MARKER),str(values[index]['TOUCHED']),str(values[index]['PAGEID']),str(random.randint(100000,99999999)),str(values[index]['PAGEID']),str(t),_revs,str(values[index]['URL']))
         print (_task)
         #tableservice.insert_entity(AZURE_TABLE, task)
